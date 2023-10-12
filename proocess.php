181879b3bb31db07a2ab937c3f676c27e4dde2c0
Eugene Ncube <philosncube@gmail.com>
2023-10-12 21:37:42 +0200

    in-progress

---
+++ proocess.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    if (!empty($name) && !empty($email) && !empty($message)) {
+        // Send an email notification to your specified email address
+        $to = "your-email@example.com";
+        $subject = "Portfolio Contact Form Submission from $name";
+        $message = "Name: $name\nEmail: $email\nMessage: $message";
+
+        mail($to, $subject, $message);
+
+        echo "Message sent successfully!";
+    } else {
+        echo "Please fill in all fields.";
+    }
+}
+?>
